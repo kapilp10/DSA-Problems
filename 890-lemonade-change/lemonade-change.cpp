e1cb3cf72c450fb6d9df7cbfe77a67d7a560522a
Kapil Paliwal <1.kapilpaliwal@gmail.com>
2026-03-25 10:53:53 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (45.05%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) 
+    {
+        int five=0;
+        int ten=0;
+
+        for(auto b : bills)
+        {
+            if(b==5)
+            {
+                five++;
+            }
+            else if(b==10)
+            {
+                if(five>0)
+                {
+                ten++;
+                five--;
+                }
+                else
+                {
+                 return false;
+                }
+            }
+
+            else
+            {
+                if(b==20)
+                {
+                    if(five>0 && ten>0)
+                    {
+                        five--;
+                        ten--;
+                    }
+                    else if(five>=3)
+                    {
+                        five-=3;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+        
+    }
+};
