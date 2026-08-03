7d5f37e39d52da823870f9c16b57905429e200f7
Kapil Paliwal <1.kapilpaliwal@gmail.com>
2026-08-03 12:17:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (85.33%) - LeetSync

--- 933-increasing-order-search-tree/increasing-order-search-tree.cpp
+++ 933-increasing-order-search-tree/increasing-order-search-tree.cpp
@@ -10,32 +10,28 @@
  * };
  */
 class Solution {
+    TreeNode* res;
+private:
+    void inorder(TreeNode* node)
+    {
+        if(!node) return;
+
+        inorder(node->left);
+
+        node->left=nullptr;
+        res->right = node;
+        res=node;
+
+
+        inorder(node->right);
+
+    }
 public:
-    // void inorder(TreeNode* res,TreeNode* root)
-    // {
-    //     if(!root) return;
-    //     inorder(res,root->left);
-    //     res->right=new TreeNode(root->val);
-    //     res->left=NULL;
-    //     res=res->right;
-    //     inorder(res,root->right);
-
-    // }
-
-    // TreeNode* increasingBST(TreeNode* root) 
-    // {
-    //     TreeNode* res = new TreeNode();
-    //     TreeNode* temp;
-    //     temp=res;
-    //     inorder(res,root);
-    //     return temp->right;  
-    // }
-
-        TreeNode* increasingBST(TreeNode* root, TreeNode* tail = NULL) {
-        if (!root) return tail;
-        TreeNode* res = increasingBST(root->left, root);
-        root->left = NULL;
-        root->right = increasingBST(root->right, tail);
-        return res;
+    TreeNode* increasingBST(TreeNode* root) 
+    {
+        TreeNode* temp=new TreeNode(0);
+        res=temp;
+        inorder(root);
+        return temp->right;  
     }
 };
