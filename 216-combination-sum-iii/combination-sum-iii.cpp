4a7f308b8305900f3c49563ab36f61934a01906e
Kapil Paliwal <1.kapilpaliwal@gmail.com>
2026-08-14 14:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.02%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -9,23 +9,16 @@ public:
         }
         for(int i=idx;i<=9;i++)
         {
-        temp.push_back(i);
-        backtrack(i+1,target-i,k,res,temp);
-        temp.pop_back();
-        // backtrack(i+1,target-i,k,res,temp);
+            temp.push_back(i);
+            backtrack(i+1,target-i,k,res,temp);
+            temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) 
     {
-        // vector<int> nums(10);
-        // for(int i=0;i<10;i++)
-        // {
-        //     nums.push_back(i);
-        // }
         vector<vector<int>> res;
         vector<int> temp;
         backtrack(1,n,k,res,temp);
         return res;
-
     }
 };
