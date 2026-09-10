a86f0862d0462e8ceb0fec4bf24794a261afd286
Kapil Paliwal <1.kapilpaliwal@gmail.com>
2026-09-11 00:42:03 +0530

    Time: 51 ms (41.28%) | Memory: 29 MB (92.31%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
         vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left=i+1;
+            int right=n-1;
+            while(left<right)
+            {
+                long long total=(long long)nums[i]+nums[left]+nums[right];
+                if(total>0)
+                {
+                    right--;
                 }
+                else if(total<0)
+                {
+                    left++;
+                }
+                else
+                {
+                    res.push_back({nums[i],nums[left],nums[right]});
+                    while(left<right && nums[left]==nums[left+1])
+                    {
+                        left++;
+                    }
+                    while(left<right && nums[right]==nums[right-1])
+                    {
+                     right--;
+                    }
+                    left++;
+                    right--;
+                }
+                
             }
         }
-        return res;        
+        return res;
+        
     }
 };
